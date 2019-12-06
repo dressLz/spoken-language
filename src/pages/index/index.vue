771617a8ddaf6c496f927b707efc1dc48b95b1e0
李卓 <1837491827@qqcom>
2019-12-06 22:17:55 +0800

    初始化项目

---
+++ src/pages/index/index.vue
@@ -0,0 +1,50 @@
+<template>
+	<view class="content">
+		<image class="logo" src="/static/logo.png"></image>
+		<view>
+			<text class="title">{{title}}</text>
+			<text class="iconfont icon-star-fill star-o"/>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				title: 'Hello'
+			}
+		},
+		onLoad() {
+			if(process.env.NODE_ENV === 'development'){
+				console.log('开发环境')
+			}else{
+				console.log('生产环境')
+			}
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style>
+	.content {
+		text-align: center;
+		height: 400rpx;
+	}
+
+	.logo {
+		height: 200rpx;
+		width: 200rpx;
+		margin-top: 200rpx;
+	}
+
+	.title {
+		font-size: 36rpx;
+		color: #8f8f94;
+	}
+	.star-o {
+		font-size: 36rpx;
+	}
+</style>
