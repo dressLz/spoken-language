134344bb9e654f2ff96640f9dfbe96e87b7d7261
李卓 <1837491827@qqcom>
2019-12-06 23:43:27 +0800

    增加环境判断，请求封装

--- src/main.js
+++ src/main.js
@@ -1,7 +1,16 @@
 import Vue from 'vue'
 import App from './App'
 
+import request from './utils/request.js'
+import api from './api/index.js'
+import url from './utils/config.js'
+
+// 阻止显示生产模式的消息
 Vue.config.productionTip = false
+
+Vue.prototype.$request = request
+Vue.prototype.$api = api
+Vue.prototype.$url = url
 
 App.mpType = 'app'
 
